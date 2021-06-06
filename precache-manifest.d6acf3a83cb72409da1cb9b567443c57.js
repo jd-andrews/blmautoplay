2530d10f790551f5ec7bb5c55f528f0a234eae5f
JD <jdandrews357@gmail.com>
2021-06-06 10:56:54 -0400

    Updates

---
+++ precache-manifest.d6acf3a83cb72409da1cb9b567443c57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd48e235299d15f05418e0003e4b4a4e",
+    "url": "/blmautoplay/index.html"
+  },
+  {
+    "revision": "7eb243812243569e2c8b",
+    "url": "/blmautoplay/static/css/main.bf3882c2.chunk.css"
+  },
+  {
+    "revision": "9c994bc6bcf30feb1f91",
+    "url": "/blmautoplay/static/js/2.78a9b91d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/blmautoplay/static/js/2.78a9b91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb243812243569e2c8b",
+    "url": "/blmautoplay/static/js/main.8341262c.chunk.js"
+  },
+  {
+    "revision": "0cef773f591dba70e522",
+    "url": "/blmautoplay/static/js/runtime-main.d556a770.js"
+  },
+  {
+    "revision": "30f928dacdc7bf6ad24959e5492822d5",
+    "url": "/blmautoplay/static/media/HTH.30f928da.png"
+  },
+  {
+    "revision": "4ccd0c8aee903b6eb3f6aaa92bab7f33",
+    "url": "/blmautoplay/static/media/blmtest.4ccd0c8a.py"
+  }
+]);
